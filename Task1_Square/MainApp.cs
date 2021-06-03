7861b48c0a8ea01ff50e0eda136d0241fea5b714
Kimbeeoh <hack513@naver.com>
2021-06-03 15:15:27 +0900

    version1

---
+++ Task1_Square/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1_Square
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("세제곱 할 숫자(정수)를 입력해주세요 : ");
+            string input = Console.ReadLine();
+            int a = int.Parse(input); 
+            int result = a * a * a;
+            Console.WriteLine("입력한 값의 세제곱은 : {0}", result);
+
+        }
+    }
+}
